99aecc8927cbdda588b7d28bef9f6715ad5d9d93
NilPointerError <gond_123@163.com>
2023-03-31 11:11:04 +0800

    commit changed data file

--- dir/data.cpp
+++ dir/data.cpp
@@ -1,38 +1,4 @@
-// 栈
-template <class T>
-class Stack{
-    int top;
-    int maxSize;
-    T* arr;
-public:
-    Stack() {
-        top = 0;
-        maxSize = 10;
-        arr = (T*)malloc(maxSize * sizeof(T));
-        this->maxSize = maxSize;
-    }
-    Stack(int maxSize) {
-        top = 0;
-        arr = (T*)malloc(maxSize * sizeof(T));
-        this->maxSize = maxSize;
-    }
-    void push(T elem){
-        if(top >= maxSize){
-            // 扩容到1.5倍
-            int oldCapacity = maxSize;
-            int newCapacity = oldCapacity + (oldCapacity >> 1);
-            arr = (T*)realloc(arr, newCapacity);
-            maxSize = newCapacity;
-            return;
-        }
-        arr[top++] = elem;
-    }
-    bool isEmpty(){
-        return top == 0;
-    }
-    bool isFull(){
-        return top >= maxSize;
-    }
 
-};
 
+
+change data.cpp
